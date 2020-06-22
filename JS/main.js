fb4c9e80239275055711141c4a56226fa1c4b32b
Jacob McDonnell <jacob@simplelittledream.com>
2020-06-22 10:13:40 -0400

    Updated side bar to have drop downs

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,9 @@
 function searchDuck(event){
     var x = document.getElementById('Search').value;
-    if (event.keyCode == 13 || event.which == 13) { 
+    if (event.keyCode == 13 || event.which == 13) {
         if (x == '/4chan'){
             fourChan()
-        } 
+        }
         else if (x == '/help'){
             help()
         }
@@ -11,4 +11,19 @@ function searchDuck(event){
 }
 function FocusOnInput(){
     document.getElementById("Search").focus();
+}
+function dropdown(theID) {
+  document.getElementById(theID).classList.toggle("show");
+}
+window.onclick = function(event) {	//when the user clicks anywhere else on the screen the menu disappears
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
 }
